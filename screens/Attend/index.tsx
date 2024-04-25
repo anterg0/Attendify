7f35a8ce1684c7180805330116e58eb8de3c52b4
anterg0 <antonxll50@gmail.com>
2024-04-25 16:07:57 +0300

    added loading to attend screen

--- screens/Attend/index.tsx
+++ screens/Attend/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { getFirestore, getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import attendanceRepository from '../../repositories/attandanceRepository';
@@ -12,6 +12,8 @@ const attendRepo = new attendanceRepository();
 const Attend = () => {
   const [user, setUser] = useState(null);
   const [hasOngoingAttendance, setHasOngoingAttendance] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [startup, setStartup] = useState(true);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -20,37 +22,62 @@ const Attend = () => {
       setUser(userDoc.data());
       if (await attendRepo.checkOngoing(auth.currentUser.uid)) setHasOngoingAttendance(true);
       else setHasOngoingAttendance(false);
+      setStartup(false);
     };
-
     fetchUser();
+    
   }, []);
 
   const handleCheckIn = async () => {
+    setLoading(true);
     await attendRepo.checkInWithFirebase(auth.currentUser.uid);
+    setLoading(false);
     setHasOngoingAttendance(true);
   };
   const handleCheckOut = async () => {
+    setLoading(true);
     await attendRepo.checkOutWithFirebase(auth.currentUser.uid);
+    setLoading(false);
     setHasOngoingAttendance(false);
   };
 
+  if (startup) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator color='#6358EC' size='large' />
+      </View>
+    );
+  }
+
+  const loading = isLoading ? { opacity: 0.5 } : {};
+
   return (
     <View style={styles.container}>
-      {user && (
-        <Text style={styles.greetingText}>
-          Hello, {user.firstName} {user.lastName}
-        </Text>
-      )}
-      {hasOngoingAttendance ? (
-        <TouchableOpacity style={styles.button} onPress={handleCheckOut}>
-          <Text style={styles.buttonText}>Check Out</Text>
-        </TouchableOpacity>
-      ) : (
-        <TouchableOpacity style={styles.button} onPress={handleCheckIn}>
-          <Text style={styles.buttonText}>Check In</Text>
-        </TouchableOpacity>
-      )}
+      <View style={[styles.container, loading]}>
+        {user && (
+          <Text style={[styles.greetingText]}>
+            Hello, {user.firstName} {user.lastName}
+          </Text>
+        )}
+        {hasOngoingAttendance ? (
+          <TouchableOpacity style={[styles.button]} onPress={handleCheckOut}>
+            <Text style={styles.buttonText}>Check Out</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity style={[styles.button]} onPress={handleCheckIn}>
+            <Text style={styles.buttonText}>Check In</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+      {isLoading && (
+      <ActivityIndicator
+        style={styles.activityIndicator}
+        size="large"
+        color="#6358EC"
+      />
+    )}
     </View>
+
   );
 };
 
@@ -70,9 +97,23 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     borderRadius: 7,
   },
+  activityIndicator: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
