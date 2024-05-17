c6e1bc0370e1d5a86ee36b8c2411309222d6e40b
anterg0 <antonxll50@gmail.com>
2024-05-17 20:03:47 +0300

    added design to requests

--- screens/RequestScreen/index.tsx
+++ screens/RequestScreen/index.tsx
@@ -4,6 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { dbDateToJsDate } from '../../services/AttendanceController/AttendanceController';
 import requestRepository from '../../repositories/requestRepository';
 import userRepository from '../../repositories/userRepository';
+import { DeserializeDate } from 'attendify_serializer';
 
 const auth = getAuth();
 const reqRepo = new requestRepository();
@@ -47,14 +48,15 @@ const RequestScreen = ({ navigation }) => {
     const { id, requestType, createdAt, userUid, status, startDate, endDate } = item;
 
     return (
-      <View style={[styles.requestItemContainer, status === 'approved' ? styles.approved : status === 'rejected' ? styles.rejected : '']}>
-        <Text>Request ID: {id}</Text>
-        <Text>Request Type: {requestType}</Text>
-        <Text>Created At: {dbDateToJsDate(createdAt)}</Text>
-        <Text>User UID: {userUid}</Text>
-        <Text>Status: {status}</Text>
-        <Text>Start Date: {dbDateToJsDate(startDate)}</Text>
-        <Text>End Date: {dbDateToJsDate(endDate)}</Text>
+      <View style={[styles.mainContainer, styles[`${status}MainContainer`]]}>
+        <View style={styles.topContainer}>
+          <Text>{`${requestType.toUpperCase()}\nFrom: ${DeserializeDate(startDate)} Until: ${DeserializeDate(endDate)}`}</Text>
+        </View>
+        <View style={[styles.bottomContainer, styles[status]]}>
+          <Text style={styles[`${status}Text`]}>
+            {status === 'onReview' ? 'in review' : status}
+          </Text>
+        </View>
       </View>
     );
   };
@@ -63,23 +65,22 @@ const RequestScreen = ({ navigation }) => {
 
     const { id, requestType, createdAt, userUid, status, startDate, endDate } = item;
     return (
-      <View style={styles.requestItemContainer}>
-        <Text>Request ID: {id}</Text>
-        <Text>Request Type: {requestType}</Text>
-        <Text>Created At: {dbDateToJsDate(createdAt)}</Text>
-        <Text>User Name: {userUid}</Text>
-        <Text>Status: {status}</Text>
-        <Text>Start Date: {dbDateToJsDate(startDate)}</Text>
-        <Text>End Date: {dbDateToJsDate(endDate)}</Text>
+      <View style={styles.mainContainer}>
+        <View style={styles.topContainer}>
+          <Text>Request Type: {requestType}</Text>
+          <Text>User Name: {userUid}</Text>
+          <Text>Start Date: {dbDateToJsDate(startDate)}</Text>
+          <Text>End Date: {dbDateToJsDate(endDate)}</Text>
+        </View>
 
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={() => handleApprove(userUid, id)}>
-            <Text style={styles.buttonText}>Approve</Text>
-          </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, {borderBottomLeftRadius: 13}]} onPress={() => handleApprove(userUid, id)}>
+              <Text style={styles.buttonText}>Approve</Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity style={[styles.button, {backgroundColor: 'red'}]} onPress={() => handleReject(userUid, id)}>
-            <Text style={styles.buttonText}>Reject</Text>
-          </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, {backgroundColor: 'red', borderBottomRightRadius: 13}]} onPress={() => handleReject(userUid, id)}>
+              <Text style={styles.buttonText}>Reject</Text>
+            </TouchableOpacity>
         </View>
       </View>
     );
@@ -113,15 +114,14 @@ const RequestScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     marginTop: 10,
   },
   button: {
+    flex: 1,
     backgroundColor: '#6358EC',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 7,
-    margin: 10,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonText: {
     color: 'white',
@@ -134,11 +134,40 @@ const styles = StyleSheet.create({
   },
   approved: {
     borderColor: '#6358EC',
-    borderWidth: 2,
+    borderWidth: 1,
+    backgroundColor: '#6358EC',
   },
   rejected: {
     borderColor: 'red',
-    borderWidth: 2,
+    borderWidth: 1,
+    backgroundColor: 'red',
+  },
+  onReview: {
+
+  },
+  approvedText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  rejectedText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  onReviewText: {
+    color: 'black',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  approvedMainContainer: {
+    borderColor: '#6358EC',
+  },
+  rejectedMainContainer: {
+    borderColor: 'red',
+  },
+  onReviewMainContainer: {
+
   },
   heading: {
     fontSize: 24,
@@ -150,12 +179,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  requestItemContainer: {
-    marginBottom: 10,
-    padding: 10,
+  mainContainer: {
+    width: 330,
+    height: 130,
     borderWidth: 1,
     borderColor: '#ddd',
-    borderRadius: 5,
+    borderRadius: 15, 
+    marginBottom: 15,
+  },
+  topContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    padding: 5,
+    alignItems: 'center',
+  },
+  bottomContainer: {
+    height: 30,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderBottomLeftRadius: 13,
+    borderBottomRightRadius: 13,
+    justifyContent: 'center',
   },
 });
 
